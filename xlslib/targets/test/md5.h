e270d8259a74217c1ba73f2699a47c184e1ea3f5
Ger Hobbelt <ger@hobbelt.com>
2010-06-22 07:46:39 +0000

    - couple of corrections for the MSVC200x README files (which were ripped from another project of mine).
    - preliminary addition of MD5-based checking for 'make check': now 'make check' will really validate
      whether the generated XLS files check out; as long as the generated output doesn't change (MD5 stays
      the same as the previously hardcoded MD5 checksum for each), we can safely say the XLS output is
      correct -- assuming, that is, we tested those XLS files before by loading them in Excel and 
      OpenOffice Calc, when we were changing the MD5 checksum strings in the test code!
      
    - added 'static library' project (clslib_lib.vcproj) to MSVC2005 -- it's a copy of the one which existed
      in the MSVC2008 build directory for a while already.
    
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@88 126565e4-af4f-46c5-a216-0262b1fa06ba

---
+++ xlslib/targets/test/md5.h
@@ -0,0 +1,77 @@
+#ifndef MD5_H
+#define MD5_H
+
+#include <systype.h>
+//#include <xlslib.h>
+
+
+
+
+#if defined(__cplusplus)
+extern "C" 
+{
+#endif
+
+
+
+struct MD5Context 
+{
+	unsigned32_t buf[4];
+	unsigned32_t bits[2];
+	unsigned32_t inp[16];
+};
+
+void MD5Init(struct MD5Context *context);
+void MD5Update(struct MD5Context *context, unsigned char const *buf, size_t len);
+void MD5Final(unsigned char digest[16], struct MD5Context *context);
+void MD5Transform(unsigned32_t buf[4], unsigned32_t const inp[16]);
+
+#define MD5_DIGEST_LEN		16
+#define MD5_HEXSTRING_SIZE	(2*MD5_DIGEST_LEN+1)
+
+
+/**
+Load the binary file specified by the path @a filepath into the dynamically allocated 
+buffer @a *buf_ref.
+
+When called, the caller must pass a reference to a variable where the buffer pointer (@a buf_ref) will
+be stored, plus a reference to a variable where the number of bytes loaded (@a buflen_ref) will
+be stored.
+
+@return 0 on success (and @a *buf_ref and @a *buflen_ref will have been set to point at the buffer
+containing the file content) or a non-zero number on error (while @a *buf_ref and @a *buflen_ref will 
+have been set to NULL and 0).
+
+@note
+Make sure to call @c free() to release the dynamically allocated buffer, once you're done perusing the
+loaded data.
+*/
+int load_file(unsigned8_t **buf_ref, size_t *buflen_ref, const char *filepath);
+/**
+Calculate the MD5 hash (printed in lower case hexadecimal) for the given file.
+
+@a md5_checksum must point to a character buffer large enough to contain the
+printed hash (@ref MD5_HEXSTRING_SIZE characters or more). @a md5_checksum_bufsize
+must specify the actual character buffer size available for @a mk_md5_4_file().
+
+@return 0 on success, non-zero on error.
+*/
+int mk_md5_4_file(char *md5_checksum, size_t md5_checksum_bufsize, const char *filepath);
+
+/**
+Check whether the given file has the given MD5 checksum (which is passed in printed 
+lowercase text in the @a md5_checksum parameter).
+
+@return 0 when the MD5 matches the file, otherwise a non-zero value is returned. The
+value -256 indicates that a load/memory failure occurred, while other non-zero
+return values are due to an MD5 mismatch.
+*/
+int check_file(const char *filepath, const char *md5_checksum);
+
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* !MD5_H */
