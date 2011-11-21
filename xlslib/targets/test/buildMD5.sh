39bef7970450a141a0eaae73e420640049defb03
David Hoerl <dhoerl@mac.com>
2011-11-21 19:52:15 +0000

    Enable last test
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@164 126565e4-af4f-46c5-a216-0262b1fa06ba

--- xlslib/targets/test/buildMD5.sh
+++ xlslib/targets/test/buildMD5.sh
@@ -3,7 +3,7 @@
 ./testC /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
 ./testCPP /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
 ./PR2859188 /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
-#./PR3076678 /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
+./PR3076678 /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
 
 
 
