4f0778c56304ef214be719378d330cdafc5d2199
David Hoerl <dhoerl@mac.com>
2011-11-21 17:23:50 +0000

    Developer MD5 generation
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@158 126565e4-af4f-46c5-a216-0262b1fa06ba

---
+++ xlslib/targets/test/buildMD5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./mainC /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
+./mainCPP /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
+./PR2859188 /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
+./PR3076678 /Volumes/Data/Users/dhoerl/Development/xlslib/svn/trunk/xlslib/targets/test
+
+
+
+
