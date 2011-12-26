c558c97884c51c6dc19bb79261c27eab40424841
David Hoerl <dhoerl@mac.com>
2011-12-26 16:42:25 +0000

    Moved __BCPLUSPLUS__ include to xlssys.h
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@203 126565e4-af4f-46c5-a216-0262b1fa06ba

--- xlslib/targets/test/mainC.c
+++ xlslib/targets/test/mainC.c
@@ -40,6 +40,13 @@
 #include "ac-config.win32.h"
 #endif
 
+#ifdef __BCPLUSPLUS__
+#  include "ac-config.win32.h"
+// I am assuming this header file is created and include automatically by MSVC.
+// Other compilers (I.e. BC++ ) don't have this, so I simply copied the file
+// from the MSC project to the RadStudio project and included it. RLN 111208
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
