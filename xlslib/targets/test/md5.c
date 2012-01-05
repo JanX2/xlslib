0067655e6fab22645511e0141444536a91afd8d4
David Hoerl <dhoerl@mac.com>
2012-01-05 21:21:28 +0000

    Include BSD strccasecmp
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@235 126565e4-af4f-46c5-a216-0262b1fa06ba

--- xlslib/targets/test/md5.c
+++ xlslib/targets/test/md5.c
@@ -31,17 +31,9 @@ Code adapted from citadel (www.citadel.org ???)
 #include <string.h>
 #endif
 
-#if defined(HAVE_STRCASECMP)
-// we're OK
-#else
-
-#if defined(HAVE_STRICMP)
-#warning Would someone please tell me what header file to include in this case? dhoerl at mac dot com
-#define strcasecmp(a, b)   stricmp(a, b)
-#else
-#define strcasecmp(a, b)   strcmp(a, b)
-#endif
-
+#if ! defined(HAVE_STRCASECMP)
+#define NET_BSD_STRCASECMP
+static int strcasecmp(const char *s1, const char *s2);
 #endif
 
 /*
@@ -534,3 +526,52 @@ char * check_file(const char *filepath, const char *md5_checksum)
 	}
 }
 
+/*
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * From NetBSD: strcasecmp.c,v 1.12 2000/01/22 22:19:20 mycroft Exp
+ */
+
+#ifdef NET_BSD_STRCASECMP
+
+#include <ctype.h>
+
+typedef unsigned char u_char;
+
+int
+strcasecmp(const char *s1, const char *s2)
+{
+	const u_char *us1 = (const u_char *)s1,
+			*us2 = (const u_char *)s2;
+
+	while (tolower(*us1) == tolower(*us2++))
+		if (*us1++ == '\0')
+			return (0);
+	return (tolower(*us1) - tolower(*--us2));
+}
+#endif
