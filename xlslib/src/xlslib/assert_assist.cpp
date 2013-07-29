d2407462216faed44c18b3655b4d4960fe0a0468
David Hoerl <dhoerl@mac.com>
2013-07-29 15:27:14 +0000

    Copyright got hosed
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@277 126565e4-af4f-46c5-a216-0262b1fa06ba

--- xlslib/src/xlslib/assert_assist.cpp
+++ xlslib/src/xlslib/assert_assist.cpp
@@ -3,7 +3,8 @@
  * This file is part of xlslib -- A multiplatform, C/C++ library
  * for dynamic generation of Excel(TM) files.
  *
-3 *
+ * Copyright 2010-2011 Ger Hobbelt All Rights Reserved.
+ *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
  *
@@ -30,6 +31,7 @@
 #include "common/systype.h"
 
 #include "xlslib/tostr.h"
+
 
 static void exception_throwing_assertion_reporter(const char *expr, const char *filename, int lineno, const char *funcname)
 {
