97cfbaf542d1744982c1229075a77c276c75ffa3
David Hoerl <dhoerl@mac.com>
2014-01-13 15:04:48 +0000

    Just starting to create example formulas
    git-svn-id: svn://svn.code.sf.net/p/xlslib/code/trunk@286 126565e4-af4f-46c5-a216-0262b1fa06ba

---
+++ xlslib/targets/test/formulas.cpp
@@ -0,0 +1,201 @@
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <xlslib.h>
+#include "formulas.h"
+
+using namespace std;
+using namespace xlslib_core;
+
+extern void formula_database(workbook &wb);
+
+#if 0
+		boolean_value_node_t *boolean(bool value);
+		integer_value_node_t *integer(signed32_t value);
+		float_value_node_t *floating_point(double value);
+		error_value_node_t *error_value(errcode_t value);
+		missing_arg_node_t *missing_arg(void);
+		text_value_node_t *text(const std::string& value);
+		text_value_node_t *text(const xlslib_strings::u16string& value);
+		
+		cell_deref_node_t *cell(const cell_t& value, cell_addr_mode_t attr, cell_op_class_t opclass = CELLOP_AS_VALUE);
+		cell_deref_node_t *cell(const cell_t& value, const worksheet* ws, cell_addr_mode_t attr, cell_op_class_t opclass = CELLOP_AS_VALUE);
+		cellarea_deref_node_t *area(const cell_t& upper_left_corner, const cell_t& lower_right_corner, cell_addr_mode_t attr, cell_op_class_t opclass = CELLOP_AS_VALUE);
+		cellarea_deref_node_t *area(const cell_t& upper_left_corner, const cell_t& lower_right_corner, const worksheet* ws, cell_addr_mode_t attr, cell_op_class_t opclass = CELLOP_AS_VALUE);
+		unary_op_node_t *op(expr_operator_code_t op, expression_node_t* arg);
+		binary_op_node_t *op(expr_operator_code_t op, expression_node_t* arg1, expression_node_t* arg2);
+		z_ary_func_node_t *f(expr_function_code_t func);
+		unary_func_node_t *f(expr_function_code_t func, expression_node_t* arg);
+		binary_func_node_t *f(expr_function_code_t func, expression_node_t* arg1, expression_node_t* arg2);
+		n_ary_func_node_t *f(expr_function_code_t func, size_t argcount, expression_node_t** arg_arr = NULL);
+		userdef_func_node_t *udf(int expr_user_function, size_t argcount = 0, expression_node_t** arg_arr = NULL);
+#endif
+
+void formulas(void)
+{
+	workbook wb;
+
+	formula_numbers(wb);
+	//formula_database(wb);
+	
+
+
+
+	const char *fName = "/Volumes/Data/Users/dhoerl/Public/formula.xls";
+	fprintf(stderr, "%s VERSION 2 \n", fName);
+	fprintf(stderr, "Dump \n");
+	wb.Dump(fName);
+	fprintf(stderr, "Dump End \n");
+}
+
+void formula_database(workbook &wb)
+{
+
+
+}
+
+void formula_database(workbook &wb)
+{
+	fprintf(stderr, "Start \n");
+	worksheet* sh = wb.sheet("DATABASE");
+	expression_node_factory_t& maker = wb.GetFormulaFactory();
+
+	cell_t *topLeft, *botRight;
+	uint32_t row, col, offset;
+	offset = 0;
+	
+	// Headers
+	for(col=1; col<=8; ++col) {
+		char year[32];
+		sprintf(year, "%d", 1999 + col);
+		cell_t *c = sh->label(0, col, year);
+		if(col==1) topLeft = c;
+	}
+
+	// Criteria
+	cell_t *c1 = sh->label(10, 2, "2001");
+	cell_t *c2 = sh->label(11, 2, ">9");
+	cellarea_deref_node_t *a2 = maker.area(*c1, *c2, CELL_RELATIVE_A1, CELLOP_AS_REFER); // Note CELLOP_AS_REFER
+	
+	// Data
+	for(row=1; row<=8; ++row) {
+		for(col=1; col<=8; ++col) {
+			cell_t *c = sh->number(row, col, row*col);
+			if(col==8 && row == 8) botRight = c;
+		}
+	}
+	
+	cellarea_deref_node_t *a1 = maker.area(*topLeft, *botRight, CELL_RELATIVE_A1, CELLOP_AS_REFER); // Note CELLOP_AS_REFER
+	integer_value_node_t *i1 = maker.integer(2);
+
+	// How to pass multiple args
+	expression_node_t *args[3];
+	args[0] = a1;
+	args[1] = i1;
+	args[2] = a2;
+
+	expression_node_t *f1 = maker.f(FUNC_DAVERAGE, (size_t)3, args);
+	sh->label(10, 0, "DAVERAGE");
+	sh->formula(10, 1, f1, true);
+	
+	cellarea_deref_node_t *b1	= new cellarea_deref_node_t(*a1);
+	cellarea_deref_node_t *b2	= new cellarea_deref_node_t(*a2);
+	integer_value_node_t *j1	= new integer_value_node_t(*i1);
+	args[0] = b1;
+	args[1] = j1;
+	args[2] = b2;
+	expression_node_t *f = maker.f(FUNC_DMAX, (size_t)3, args);
+	sh->label(11, 0, "DMAX");
+	sh->formula(11, 1, f, true);
+}
+
+#if 0
+void formulas(void)
+{
+
+	fprintf(stderr, "Start \n");
+	workbook wb;
+	worksheet* sh = wb.sheet("FORMULAS");
+	expression_node_factory_t& maker = wb.GetFormulaFactory();
+
+	// add two numbers
+	if(0){
+		cell_t *c1 = sh->number(0, 0, 1);
+		cell_deref_node_t *cd1 = maker.cell(*c1, CELL_RELATIVE_A1);
+		cell_t *c2 = sh->number(0, 1, 1);
+		cell_deref_node_t *cd2 = maker.cell(*c2, CELL_RELATIVE_A1);
+
+		expression_node_t *en = maker.f(FUNC_SUM, cd1, cd2);
+		sh->formula(0, 32, en, true);
+	}
+
+	// add from 1 to 30 numbers
+	uint32_t row, col, offset;
+	offset = 0;
+	for(row=0; row<32; ++row) {
+		expression_node_t *cells[row];
+		for(col=0; col<row; ++col) {
+			cell_t *c = sh->number(row+offset, col, 1);
+			expression_node_t *cdn = maker.cell(*c, CELL_RELATIVE_A1);
+			cells[col] = cdn;
+		}
+		if(row == 0) continue;
+
+		expression_node_t *f = maker.f(FUNC_SUM, (size_t)col, cells); // (expression_node_t **)
+if(row==31) {
+	sh->formula(row, 32, f);
+	printf("WRITE FORMULA\n");
+}
+	}
+	
+	// concatenate from 1 to 30 strings
+	offset = 40;
+	if(0)for(row=0; row<31; ++row) {
+		expression_node_t *cells[row];
+		for(col=0; col<row; ++col) {
+			char foo[2] = { static_cast<char>('A' + (char)col), (char)0 };
+			cell_t *c = sh->label(row+offset, col, foo);
+			expression_node_t *cdn = maker.cell(*c, CELL_RELATIVE_A1);
+			cells[col] = cdn;
+		}
+		if(row == 0) continue;
+
+		expression_node_t *f = maker.f(FUNC_CONCATENATE, (size_t)col, cells); // (expression_node_t **)
+		sh->formula(row+offset, 32, f);
+	}
+
+
+#if 0
+	// row & col
+	cell_t *a1t = sh->number(0, 0, -10);
+				  sh->number(1, 0,  20);
+	cell_t *a1b = sh->number(2, 0, -30);
+	
+	cell_t *a2t = sh->number(0, 1,  10);
+				  sh->number(1, 1, -20);
+	cell_t *a2b = sh->number(2, 1,  30);
+
+	cellarea_deref_node_t *a1 = maker.area(*a1t, *a1b, CELL_RELATIVE_A1, CELLOP_AS_ARRAY);
+	cellarea_deref_node_t *a2 = maker.area(*a2t, *a2b, CELL_RELATIVE_A1, CELLOP_AS_ARRAY);
+	expression_node_t *i1 = maker.op(OP_GT, a1, a2);
+
+
+	cellarea_deref_node_t *aa1 = maker.area(*a1t, *a1b, CELL_RELATIVE_A1, CELLOP_AS_ARRAY);
+	cellarea_deref_node_t *aa2 = maker.area(*a2t, *a2b, CELL_RELATIVE_A1, CELLOP_AS_ARRAY);
+	expression_node_t *args[3] = { i1, aa1, aa2 };
+	expression_node_t *i2 = maker.f(FUNC_IF, 3, args, CELLOP_AS_ARRAY);
+	expression_node_t *pExpFormula = maker.f(FUNC_SUM, i2, CELLOP_AS_ARRAY);
+
+	sh->formula(0, 2, true, pExpFormula, true);
+#endif
+
+	const char *fName = "/Volumes/Data/Users/dhoerl/Public/formula.xls";
+	fprintf(stderr, "%s VERSION 2 \n", fName);
+	fprintf(stderr, "Dump \n");
+	wb.Dump(fName);
+	fprintf(stderr, "Dump End \n");
+}
+#endif
+
+
---
+++ xlslib/targets/test/formulas.h
@@ -0,0 +1,16 @@
+//
+//  formulas.h
+//  xlsLibTester
+//
+//  Created by David Hoerl on 1/7/14.
+//
+//
+
+#ifndef __xlsLibTester__formulas__
+#define __xlsLibTester__formulas__
+
+#include <iostream>
+
+void formulas(void);
+
+#endif /* defined(__xlsLibTester__formulas__) */
