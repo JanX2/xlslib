53f680ddc63105796aff24c93dd5a62bc8624780
Marcel Schneider <marcel@coopmasters.de>
2017-12-16 22:21:18 +0100

    xlslib builds with cmake on linux

---
+++ xlslib/src/common/xlconfig.h.cmake
@@ -0,0 +1,32 @@
+#define PACKAGE_VERSION "2.5.0"
+#define PACKAGE_NAME "xlslib"
+
+#cmakedefine HAVE_CRTDBG_H
+#cmakedefine HAVE_CRTDBG_H
+#cmakedefine HAVE_CRT_EXTERNS_H
+#cmakedefine HAVE_CTYPE_H
+#cmakedefine HAVE_DLFCN_H
+#cmakedefine HAVE_ICONV
+#cmakedefine HAVE_INTTYPES_H
+#cmakedefine HAVE_MEMORY_H
+#cmakedefine HAVE_STDINT_H
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_STRINGS_H
+#cmakedefine HAVE_STRING_H
+#cmakedefine HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_TYPES_H
+#cmakedefine HAVE_UNISTD_H
+#cmakedefine HAVE_WCHAR_H
+#cmakedefine HAVE_WINDOWS_H
+
+#cmakedefine HAVE_MEMCHR
+#cmakedefine HAVE_MEMMOVE
+#cmakedefine HAVE_MEMSET
+#cmakedefine HAVE_SNPRINTF
+#cmakedefine HAVE_STRCASECMP
+#cmakedefine HAVE_STRCHR
+#cmakedefine HAVE_STRCMP
+#cmakedefine HAVE_STRICMP
+#cmakedefine HAVE_VSNPRINTF
+#cmakedefine HAVE__SNPRINTF
+#cmakedefine HAVE__VSNPRINTF
